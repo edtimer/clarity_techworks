6aa7cf130512fcffec0fd8ff17e8fe17a1b185f4
edtimer <b4timer@protonmail.com>
2023-06-26 05:21:24 +0100

    fix: add missing controller import in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
